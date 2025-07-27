9ddeef554ea538958d4bbec7ce6d681037326704
SamanPrasad <samanseedr2@gmail.com>
2025-07-28 03:05:47 +0530

    CORS

--- app/api/payhere/route.ts
+++ app/api/payhere/route.ts
@@ -23,7 +23,7 @@ export async function POST() {
     headers: {
       "content-type": "application/json",
       "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "https://payhereclient.vercel.app/",
+      "Access-Control-Allow-Origin": "https://payhereclient.vercel.app",
       "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
       "Access-Control-Allow-Headers":
         "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
