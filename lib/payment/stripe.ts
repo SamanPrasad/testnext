7a59c704118a630a70d5a80b0e91930d4d006d12
SamanPrasad <samanseedr2@gmail.com>
2025-07-29 01:43:54 +0530

    env

--- lib/payment/stripe.ts
+++ lib/payment/stripe.ts
@@ -4,9 +4,7 @@ let stripe: Stripe | null = null;
 
 export function createStripe() {
   if (!stripe) {
-    stripe = new Stripe(
-      "sk_test_51RoUPb7rGzlKVazq1l0fwlyPemRdVgydsRMGF9o1pNTkNf6MSqgHUzVO4J7U65lS4KoTGKyt9VK9OGYv6GQSGhzG008hMDymdK"
-    );
+    stripe = new Stripe(process.env.STRIPE as string);
   }
 
   return stripe;
