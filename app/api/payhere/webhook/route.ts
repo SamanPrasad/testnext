dcb8b7eff1d0d8b295dd57632963f552724c8788
SamanPrasad <samanseedr2@gmail.com>
2025-07-28 13:43:54 +0530

    changes new

--- app/api/payhere/webhook/route.ts
+++ app/api/payhere/webhook/route.ts
@@ -4,6 +4,6 @@ import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   const body = await req.json();
   redirect(
-    `/payhere?merchant_id=${body.merchant_id}&status_message=$status_message=${body.status_message}`
+    `/payhere?merchant_id=${body.merchant_id}&status_message=${body.status_message}`
   );
 }
